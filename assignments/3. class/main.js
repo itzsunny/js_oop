9611933cf2eda4794bf24daf1942e47772f3a9f8
sunny <sunnychoudhury007@gmail.com>
2019-10-07 10:36:04 +0530

    update assignments

--- assignments/3. class/main.js
+++ assignments/3. class/main.js
@@ -1,13 +1,54 @@
-class BookList {
-    constructor(){
-        this.readBooks = 0;
-        this.notRead = 0;
-    }
-    book();
-}
+
+
 class Book {
-    constructor(){
-        
+    constructor (title, genre, author, read, date) {
+      this.title = title;
+      this.genre = genre;
+      this.author = author;
+      this.read = read;
+      this.date = new Date(date) || Date.now();
     }
-   this.readCount = 0
+  }
+    
+  class Booklist {
+    constructor (book) {
+      this.books = [];
+      this.books.push(book);
+    }
+ markedAsRead () {
+    return this.books.filter (book => book.read);
+  }
+
+ markedNotRead () {
+    return this.books.filter (book => !book.read);
+  }
+
+ currentBook () {
+    for (let i = 0; i < this.books.length; ++i) {
+      if (!this.books[i].read) {
+        return this.books[i];
+      }
+    }
+  }
+
+  nextBook () {
+    return this.books[this.books.indexOf (this.currentBook) + 1];
+  }
+
+  lastBook () {
+    return this.books[this.books.indexOf (this.currentBook) - 1];
+  }
+
+  allBooks () {
+    return this.books;
+  }
+
+  add (book) {
+    this.books.push(book);
+  }
+
+  finishCurrentBook () {
+    this.currentBook.date = Date.now();
+    this.currentBook.read = true;
+  }
 }
